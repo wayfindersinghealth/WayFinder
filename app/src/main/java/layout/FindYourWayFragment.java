ed44bb56ed223c683ea69f23887196b39945a5d7
151292h <151292h@mymail.nyp.edu.sg>
2017-09-18 13:03:42 +0800

    Added Test Map to MapBox.

--- app/src/main/java/layout/FindYourWayFragment.java
+++ app/src/main/java/layout/FindYourWayFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
