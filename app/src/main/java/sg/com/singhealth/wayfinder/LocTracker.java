11bbff6753c8b4a3578724a34c5843c88d4ca90a
L31106 <155121j@mymail.nyp.edu.sg>
2017-10-13 11:04:51 +0800

    Updated Mapbox Style

---
+++ app/src/main/java/sg/com/singhealth/wayfinder/LocTracker.java
@@ -0,0 +1,27 @@
+package sg.com.singhealth.wayfinder;
+
+/**
+ * Created by L31106 on 10/12/2017.
+ */
+
+public class LocTracker {
+    String LocationName;
+    int counter;
+
+    public String getLocationName() {
+        return LocationName;
+    }
+
+    public void setLocationName(String locationName) {
+        LocationName = locationName;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+}
